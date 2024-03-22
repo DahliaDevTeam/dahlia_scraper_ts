2623dd1e07c3aaac80174351ce668d902270b56b
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-03-22 18:48:06 +0100

    scrapeAllSites function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { EventbriteWebScraper } from "./features/scraper/data/factory/eventbrite_web_scraper";
 import { FeverWebScraper } from "./features/scraper/data/factory/fever_web_scraper";
+import { EventEntity } from "./features/scraper/domain/entities/event_entity";
 import { ScrapedSite } from "./features/scraper/domain/enums/scraped_sites_enum";
 import { ScraperFactory } from "./features/scraper/domain/factory/scraper_factory";
 
@@ -7,3 +8,13 @@ export const DahliaScraper = new ScraperFactory();
 
 DahliaScraper.register(ScrapedSite.eventbrite, new EventbriteWebScraper());
 DahliaScraper.register(ScrapedSite.fever, new FeverWebScraper());
+
+export async function scraperAllSites(): Promise<EventEntity[]> {
+
+    const res = await Promise.all(
+        Object.values(ScrapedSite)
+            .map((site) => DahliaScraper.get(site).scrape())
+    );
+    return res.flat();
+
+}

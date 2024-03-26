3587bffe0e0bad71f651b9d083836744da495c81
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-03-26 18:53:43 +0100

    index export event entity

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { EventEntity } from "./features/scraper/domain/entities/event_entity";
 import { ScrapedSite } from "./features/scraper/domain/enums/scraped_sites_enum";
 import { ScraperFactory } from "./features/scraper/domain/factory/scraper_factory";
 
+export * from "./features/scraper/domain/entities/event_entity"
+
 export const DahliaScraper = new ScraperFactory();
 
 DahliaScraper.register(ScrapedSite.eventbrite, new EventbriteWebScraper());
